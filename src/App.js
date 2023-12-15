adfc6584e2881b58215fb82aed28bc86a1d930a6
Rahul Phatangare <rahul.phatangare@heinekingmedia.de>
2023-12-15 12:11:57 +0530

    custom theme toggle working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme"; 
-import {CssBaseline, ThemeProvider} from '@mui/material'
+import {CssBaseline, ThemeProvider} from '@mui/material';
+
+import Topbar from "./scenes/global/Topbar";
 function App() {
   const [theme,colorMode] = useMode();
   return (
